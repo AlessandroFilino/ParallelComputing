8deb13ca70da6f7bf4402fc5774743285d7af284
AlessandroFilino <65735750+AlessandroFilino@users.noreply.github.com>
2023-12-13 15:36:51 +0100

    add test

--- MidTermExam/Results/results.py
+++ MidTermExam/Results/results.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt 
 
 def sequenziale_parallelo_8():
-    num_threads = [1, 2, 3, 4, 5, 6, 7, 8]
+    #MAC
+    #num_threads = [1, 2, 3, 4, 5, 6, 7, 8]
+    #tempo_esecuzione_sequenziale = [3964.3]
+    #tempo_esecuzione_parallelo = [3635.43, 1870.2, 1274.33, 952.182, 763.445, 644.883, 618.588, 595.119]
+    num_threads = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
     tempo_esecuzione_sequenziale = [3964.3]
-    tempo_esecuzione_parallelo = [3635.43, 1870.2, 1274.33, 952.182, 763.445, 644.883, 618.588, 595.119]
-
+    tempo_esecuzione_parallelo = [4461.75, 2236.64, 1493.41, 1124.49, 901.942, 756.772, 649.167, 594.315, 521.07, 469.617, 434.817, 398.885, 373.993, 349.434, 327.47, 309.461, 286.749, 279.44, 268.801, 250.751, 236.494, 235.226, 219.68, 215.853, 209.417, 199.965, 194.025, 186.961, 184.563, 174.373, 171.084, 164.497]
     plt.scatter(num_threads[0], tempo_esecuzione_sequenziale, color='red', marker='o', label='Sequenziale')
     plt.plot(num_threads[0], tempo_esecuzione_sequenziale, color='red', linestyle='-', linewidth=2)
 
@@ -20,10 +23,13 @@ def sequenziale_parallelo_8():
     plt.show()
 
 def speed_up_sequenziale_parallelo_8():
-    num_threads = [1, 2, 3, 4, 5, 6, 7, 8]
+    #MAC
+    #num_threads = [1, 2, 3, 4, 5, 6, 7, 8]
+    #tempo_esecuzione_sequenziale = [3964.3]
+    #tempo_esecuzione_parallelo = [3635.43, 1870.2, 1274.33, 952.182, 763.445, 644.883, 618.588, 595.119]
+    num_threads = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
     tempo_esecuzione_sequenziale = [3964.3]
-    tempo_esecuzione_parallelo = [3635.43, 1870.2, 1274.33, 952.182, 763.445, 644.883, 618.588, 595.119]
-
+    tempo_esecuzione_parallelo = [4461.75, 2236.64, 1493.41, 1124.49, 901.942, 756.772, 649.167, 594.315, 521.07, 469.617, 434.817, 398.885, 373.993, 349.434, 327.47, 309.461, 286.749, 279.44, 268.801, 250.751, 236.494, 235.226, 219.68, 215.853, 209.417, 199.965, 194.025, 186.961, 184.563, 174.373, 171.084, 164.497]
     speed_up = []
     for parallelo in tempo_esecuzione_parallelo:
         risultato = tempo_esecuzione_sequenziale[0] / parallelo
